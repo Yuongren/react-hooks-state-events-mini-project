7488e2b254e41291dc1dee15302651985292ec49
gitYoungren <youngrengitonga@gmail.com>
2023-01-19 09:11:18 +0300

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React,{useState} from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
@@ -8,12 +8,22 @@ console.log("Here's the data you're working with");
 console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [tasksList, setTasksList] = useState(TASKS)
+  function onTaskFormSubmit(newTask){
+
+    const updatedTasks = [
+      ...tasksList,
+     newTask
+    ]
+    setTasksList(updatedTasks)
+  }
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      
+      <CategoryFilter categories={CATEGORIES} tasks={TASKS} setTasks={setTasksList}/>
+      <NewTaskForm categories={CATEGORIES} onTaskFormSubmit={onTaskFormSubmit}/>
+      <TaskList tasks={tasksList} setTasks={setTasksList}/>
     </div>
   );
 }
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,32 @@
-import React from "react";
+// import React from "react";
+// import { useState } from "react";
 
-function TaskList() {
+// function TaskList(props) {
+//   return (
+//     <div className="tasks">
+//       { props.task /* display a list of tasks using Task component */}
+//     </div>
+//   );
+// }
+  
+// export default TaskList;
+
+
+import React from "react";
+import Task from "./Task";
+
+function TaskList({tasks, setTasks}) {
+  
+  function handleDeleteTask(taskName){
+    const updatedTasks = tasks.filter(task=>task.text !== taskName)
+    setTasks(updatedTasks)
+  }
+  
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+      {/* display a list of tasks using Task component */
+      tasks.map(task=><Task key={task.text} category={task.category} text={task.text} handleDeleteTask = {handleDeleteTask}/>)
+      }
     </div>
   );
 }
